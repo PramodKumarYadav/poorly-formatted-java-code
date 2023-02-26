3b1e90eb39fb5605cef270fde623b0aa82f500b7
pramodkumaryadav <pramodyadav027@gmail.com>
2023-02-26 20:13:49 +0100

    again pushing ill formatted code

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,10 @@
-public class MainClass {
-  public static void main(String[] args) {
-    System.out.println("poorly formatted java code");
-  }
+public
+class
+MainClass
+{ public static void main(String[] args) {
+    System.out.println("poorly formatted java code"); }
 
   @Override
   public String toString() {
     System.out.println("This is a statement from the toString method");
-    return "MainClass{}";
-  }
-}
+    return "MainClass{}"; }}

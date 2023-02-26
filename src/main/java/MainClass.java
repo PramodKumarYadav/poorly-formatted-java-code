cff1c8a081807a862981d5a72cb7cb973d48f7cb
pramodkumaryadav <pramodyadav027@gmail.com>
2023-02-26 20:04:35 +0100

    added pretty poorly formatted java code

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+public
+class
+MainClass{
+    public
+    static void main   (String[] args){
+        System.out.println("poorly formatted java code");
+    }
+
+    @Override
+    public 
+    String 
+    toString    (){
+        System.out.println(
+                "This is a statement from the toString method"
+        );
+        return
+                "MainClass{}"; }}
